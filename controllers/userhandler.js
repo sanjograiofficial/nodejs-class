11e992ad8741d5c7a2fee5b8e3e86c26f5744b76
sanjograiofficial <sanjograiofficial@gmail.com>
2026-05-15 13:58:43 +0545

    Controller

--- controllers/userhandler.js
+++ controllers/userhandler.js
@@ -20,8 +20,43 @@ const getUserById = (req, res) => {
     user,
   });
 };
-const createUser = (req, res) => {};
-const updateUser = (req, res) => {};
-const deleteUser = (req, res) => {};
+const createUser = (req, res) => {
+  const user = req.body;
+  AllUsers.push(user);
+  res.json({
+    message: "Created user successfully",
+    user,
+  });
+};
+const updateUser = (req, res) => {
+  const userId = req.params.id;
+  const update = req.body;
+  const user = AllUsers.find((item) => item.id == userId);
+  if (!user) {
+    res.status(404).json({
+      message: "User not found",
+    });
+  }
+  user.name = update.name;
+  user.email = update.email;
+
+  res.json({
+    message: "User updated successfully",
+    user,
+  });
+};
+const deleteUser = (req, res) => {
+  const userId = req.params.id;
+  const user = AllUsers.findIndex((item) => item.id == userId);
+  if (!user) {
+    res.status(404).json({
+      message: "User not found",
+    });
+  }
+  AllUsers.splice(user, 1);
+  res.json({
+    message: "User deleted successfully",
+  });
+};
 
 export { getAllUsers, getUserById, createUser, updateUser, deleteUser };
